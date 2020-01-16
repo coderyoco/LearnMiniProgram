d1b87173e8fb846db37769134b0bd3965a6255bf
coder <18937581633@163.com>
2020-01-16 15:51:45 +0800

    弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,74 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '正在加载',
+      duration: 3000,
+      icon: 'loading',
+      //image: '/assets/icon/icon.png',
+      mask: true,
+      success: function() {
+        console.log('展示弹窗成功');
+      },
+      fail: function() {
+        console.log('接口调用失败');
+      },
+      complete: function() {
+        console.log('接口调用成功的回调函数');
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容，哈哈哈',
+      //showCancel: false,
+      cancelText: '重置',
+      cancelColor: '#ff5777',
+      success: function(res) {
+        console.log(res);
+        if(res.confirm) {
+          console.log('用户点击了确定');
+        }
+        if(res.cancel) {
+          console.log('用户点击了取消');
+        }
+      }
+    })
   },
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    setTimeout(() => {
+      //必须手动hideLoading才能让loading消失
+      wx.hideLoading()
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction() {
+    wx.showActionSheet({
+      itemList: ['拍照', '相册'],
+      itemColor: '#ff5888',
+      success: function(res) {
+        console.log(res);
+        switch (res.tapIndex) {
+          case 0:
+            break;
+          case 1:
+            console.log('当前点击了相册');
+            break;
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //分享
+  onShareAppMessage: function(options) {
+    return {
+      title: '猪猪大作战',
+      path: 'pages/index/index',
+      imageUrl: 'http://b-ssl.duitang.com/uploads/item/201705/13/20170513174753_Pv42r.jpeg'
+    }
   }
 })
